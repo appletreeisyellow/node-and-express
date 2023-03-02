42fe31f3d6b5ba2752559292a12a2f7b7ddc15b3
Chunchun <14298407+appletreeisyellow@users.noreply.github.com>
2023-03-01 22:11:58 -0600

    chore: update static file

--- myApp.js
+++ myApp.js
@@ -1,7 +1,17 @@
 let express = require('express');
 let app = express();
 
-app.use(express.static(`${__dirname}/public`))
+/**
+ * Serve Static Assets
+ *
+ * To serve static files such as images, CSS files, and JavaScript files,
+ * use the express.static built-in middleware function in Express.
+ *
+ * Middleware are functions that intercept route handlers
+ * A middleware needs to be mounted using the method app.use(path, middlewareFunction).
+ *
+*/
+app.use("/public", express.static(__dirname + `/public`))
 
 app.get("/", (req, res) => {
   res.sendFile(`${__dirname}/views/index.html`)
